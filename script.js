2bbf4cc8cc82f74974774d51bd2995d424534e29
ChrisFennek <christian.gebhardt@online.de>
2020-11-11 23:50:02 +0100

    API

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 // Unsplash API
 
-const apiUrl = `https://api.unsplash.com/photos/?client_id=YOUR_ACCESS_KEY`;
+const count = 10;
+const apiKey = "Q5xWXyMxMtmXWVDhcmbrze_AsbWThJ5syG-k4-O6VUo";
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&${count}=`;
+
+// Get Pics
+
+async function getPhotos() {
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+
+  }
+}
+
+getPhotos();
